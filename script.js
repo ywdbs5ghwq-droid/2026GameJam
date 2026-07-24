c70973084be12a7988686a07ef7ce92fef259eda
roboenz <ywdbs5ghwq@privaterelay.appleid.com>
2026-07-24 13:13:38 +0100

    Stop personalDeck being added to "play" array
    Also stopped its text from being erased by adding
    and empty <p> element that is manipulated instead
    of its main text.

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-// this file is to mess around and learn some javascript on the go. Delete everything afterwards, ellie's got it.
 const card1 = document.querySelector("#card1");
 const card2 = document.querySelector("#card2");
 const card3 = document.querySelector("#card3");
@@ -12,17 +11,19 @@ hand.push(card1, card2, card3, card4);
 const cards = document.querySelectorAll(".card");
 
 cards.forEach( (card) => {
-    card.addEventListener("mouseenter", () => {
-        card.setAttribute("style", "border-color: red;")
-    });
-    card.addEventListener("mouseleave", () => {
-        card.setAttribute("style", "border-color: blue;")
-    });
-    card.addEventListener("click", () => {
-        if (play.includes(card)) { play.splice(play.indexOf(card), 1 ) }
-        else {play.push(card)};
-        displayPos();
-    }); 
+    if (card.id !== "personalDeck") {
+        card.addEventListener("mouseenter", () => {
+            card.setAttribute("style", "border-color: red;")
+        });
+        card.addEventListener("mouseleave", () => {
+            card.setAttribute("style", "border-color: blue;")
+        });
+        card.addEventListener("click", () => {
+            if (play.includes(card)) { play.splice(play.indexOf(card), 1 ) }
+            else {play.push(card)};
+            displayPos();
+        }); 
+    }
 })
 
 function displayPos() {
