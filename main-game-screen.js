9e6e8ea260817f9c71538671102ab825d71acea5
roboenz <ywdbs5ghwq@privaterelay.appleid.com>
2026-07-24 19:57:35 +0100

    Add functionality to shop button
    Step one of merging script.js and main-game-screen.js
    files.

--- main-game-screen.js
+++ main-game-screen.js
@@ -1,25 +1,33 @@
-let happinessMeter = document.getElementById("happiness-meter");
-let timer = document.getElementById("timer");
+//declaring all of the variables needed to manipulate content on main screen and shop screen. Constant because they have no reason to change.
+const happinessMeter = document.getElementById("happiness-meter");
+const timer = document.getElementById("timer");
 
-let card1 = document.getElementById("card1");
-let card2 = document.getElementById("card2");
-let card3 = document.getElementById("card3");
-let card4 = document.getElementById("card4");
+const shopButton = document.querySelector("#shopButton");
+const shop = document.querySelector(".shop");
+const content = document.querySelector(".content");
 
-let one = "1";
-let two = "2";
-let three = "3";
-let four = "4";
-let five = "5";
-let six = "6";
-let seven = "7";
-let eight = "8";
+const card1 = document.getElementById("card1");
+const card2 = document.getElementById("card2");
+const card3 = document.getElementById("card3");
+const card4 = document.getElementById("card4");
 
+// syntax for cards: name, description, time decrease, happy increase/decrease
+let one = ["ONE", "<description>", 1, 1];
+let two = ["TWO", "<description>", 1, 1];
+let three = ["THREE", "<description>", 1, 1];
+let four = ["FOUR", "<description>", 1, 1];
+let five = ["FIVE", "<description>", 1, 1];
+let six = ["SIX", "<description>", 1, 1];
+let seven = ["SEVEN", "<description>", 1, 1];
+let eight = ["EIGHT", "<description>", 1, 1];
+let ph1 = ["placeholder1"];
+let ph2 = ["placeholder2"];
+let ph3 = ["placeholder3"];
 
-let personal = [one, two, three, four];
+let personal = [one, two, three, four, ph1, ph2, ph3];
 let hand = [];
 let play = [];
-let points = 0;
+let money = 0; // money: flat value of level + bonus (depends on happiness & time over minimum at the end of each round)
 let time = 60;
 let happy = 100;
 
@@ -39,3 +47,10 @@ function initializeBoard() {
 function gameLoop() {
 
 }
+
+// below this point handles the html elements on the screen and gives them functionality
+
+shopButton.addEventListener("click", () => {
+    shop.setAttribute("style", "display: none;")
+    content.setAttribute("style", "display: flex;")
+})
