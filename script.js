b60a41cdbd0cd63b29b8cc342c0c8b373f72e0fe
roboenz <ywdbs5ghwq@privaterelay.appleid.com>
2026-07-24 16:51:24 +0100

    Change css for better layout in main and shop
    Also added button in shop for navigation back to main
    screen. CSS changes mean less flexbox in main screen
    and no scrolling/sizing issues.

--- script.js
+++ script.js
@@ -3,6 +3,10 @@ const card2 = document.querySelector("#card2");
 const card3 = document.querySelector("#card3");
 const card4 = document.querySelector("#card4");
 
+const shopButton = document.querySelector("#shopButton");
+const shop = document.querySelector(".shop");
+const content = document.querySelector(".content");
+
 let hand = [], play = [];
 
 hand.push(card1, card2, card3, card4);
@@ -31,3 +35,8 @@ function displayPos() {
         card.lastChild.textContent = (play.indexOf(card) == -1 ) ? "" : "Position "+(play.indexOf(card)+1);
     })
 }
+
+shopButton.addEventListener("click", () => {
+    shop.setAttribute("style", "display: none;")
+    content.setAttribute("style", "display: flex;")
+})
