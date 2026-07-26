be0803f875d0001d393cdbd1ffdd0f43a73c6e29
roboenz <ywdbs5ghwq@privaterelay.appleid.com>
2026-07-26 12:44:00 +0100

    Made a game that plays itself.
    Add interactivity and card-comboes through if/else
    statements.

--- main-game-screen.js
+++ main-game-screen.js
@@ -11,18 +11,18 @@ const card2 = document.getElementById("card2");
 const card3 = document.getElementById("card3");
 const card4 = document.getElementById("card4");
 
-// syntax for cards: name, description, time decrease, happy increase/decrease
-let one = ["ONE", "<description>", 1, 1];
-let two = ["TWO", "<description>", 1, 1];
-let three = ["THREE", "<description>", 1, 1];
-let four = ["FOUR", "<description>", 1, 1];
-let five = ["FIVE", "<description>", 1, 1];
-let six = ["SIX", "<description>", 1, 1];
-let seven = ["SEVEN", "<description>", 1, 1];
-let eight = ["EIGHT", "<description>", 1, 1];
-let ph1 = ["PLACEHOLDER1"];
-let ph2 = ["PLACEHOLDER2"];
-let ph3 = ["PLACEHOLDER3"];
+// syntax for cards: name, description, time decrease
+let one = ["ONE", "<description>", 3];
+let two = ["TWO", "<description>", 3];
+let three = ["THREE", "<description>", 3];
+let four = ["FOUR", "<description>", 3];
+let five = ["FIVE", "<description>", 3];
+let six = ["SIX", "<description>", 3];
+let seven = ["SEVEN", "<description>", 3];
+let eight = ["EIGHT", "<description>", 3];
+let ph1 = ["PLACEHOLDER1", "<description>", 3];
+let ph2 = ["PLACEHOLDER2", "<description>", 3];
+let ph3 = ["PLACEHOLDER3", "<description>", 3];
 
 let personal = [one, two, three, four, ph1, ph2, ph3];
 let hand = [];
@@ -44,10 +44,20 @@ function initializeBoard() {
     console.log("Operation \"initializeBoard()\" complete!");
     console.log("Personal now contains: "+personal);
     console.log("Hand now contains: "+hand)
+    timer.textContent = "TIME LEFT: "+time + "min";
+    gameLoop()
 }
 
 function gameLoop() {
-
+    while (time > 0) {
+        timer.textContent = "TIME LEFT: "+time + "min";
+        happinessMeter.textContent = happy;
+        dealHand()
+        playHand()
+    }
+    timer.textContent = "TIME LEFT: "+time + "min";
+    happinessMeter.textContent = happy;
+    alert("You won!")
 }
 
 function dealHand() {
@@ -56,6 +66,14 @@ function dealHand() {
         hand.push(personal[rand]);
         personal.splice(rand, 1);
     }
+}
+
+function playHand() {
+    for (i=0; i<4; i++) {
+        time = time - hand[i][2]
+    }
+    console.log(time)
+    dealHand()
 }
 
 document.addEventListener("keydown", () => {
